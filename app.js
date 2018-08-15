03bdca857a3eb579b54200c06af98229fc469481
butlariz <laris.santana09@gmail.com>
2018-08-15 15:03:16 -0300

    tudo

--- app.js
+++ app.js
@@ -15,6 +15,34 @@ function loadNameUser(){
   });
 }
 
+// Função botão editar
+function editPost(idPost) {
+  console.log("chamou função edit")
+  $('button[data-edit-id="edit-' + idPost + '"]').click(function(){
+    var spanEdit = $('#span-post-' + idPost)[0];
+    var btnEdit = $(this)[0];
+      if (btnEdit.value === "change") {
+        spanEdit.setAttribute("contenteditable","true");
+        spanEdit.setAttribute("style","border:1px solid #ababab");
+        btnEdit.value = "save";
+        btnEdit.innerHTML = '<i class="far fa-save"></i>';
+      } else if (btnEdit.value === "save"){
+        spanEdit.removeAttribute("contenteditable");
+        spanEdit.removeAttribute("style");
+        btnEdit.value = "change";
+        btnEdit.innerHTML = '<i class="fas fa-pencil-alt"></i>';
+          // colocar texto no banco de dados
+        btnEdit.click(function(){
+          var newPost = spanEdit.val();
+          var postFromDB = database.ref("posts/" + USER_ID + "/" + idPost).updates({
+            text: newPost
+          });
+        })
+      }
+      return false;    
+  });
+}
+
 // Links do menu
 $(".a-home").click(function() {
   window.location = "home.html?id=" + USER_ID + "&";
--- profile.js
+++ profile.js
@@ -2,7 +2,7 @@ var PROFILE_ID = findProfileId();
 
 $(document).ready(function(){
   console.log(USER_ID);
-  console.log(PROFILE_ID)
+  console.log(PROFILE_ID);
   loadNameProfile();
   loadButton();
 
@@ -16,13 +16,17 @@ $(document).ready(function(){
           <li>
             <div data-task-id=${childKey} />
               <span>${childData.text}</span>
-              <footer><button data-btn-id="btn-${childKey}"> Delete </button></footer>
+              <footer>
+              <button data-btn-id="btn-${childKey}"> <i class="far fa-trash-alt"></i>  </button>
+              <button data-edit-id="edit-${childKey}" value="change"> <i class="fas fa-pencil-alt"></i> </button>
+              </footer>
             </div>
           </li>`);
         $(`button[data-btn-id="btn-${childKey}"]`).click(function(){
           database.ref("posts/" + PROFILE_ID + "/" + childKey).remove();
           $(this).parent().remove();
         })
+      editPost(childKey);
     });
   });
 
@@ -102,3 +106,29 @@ function loadButton(){
     });
   }
 }
+
+$(".publish").click(function(event){
+  event.preventDefault();
+  var newPost = $(".publish-input").val();
+  var postFromDB = database.ref("posts/" + USER_ID).push({
+    text: newPost
+  });
+  $(".input-list").prepend(`
+  <li>
+    <div data-post-id="${postFromDB.key}">
+      <span id="span-post-${postFromDB.key}">${newPost}</span>
+      <footer>
+      <button data-btn-id="btn-${postFromDB.key}"> <i class="far fa-trash-alt"></i> </button>
+      <button data-edit-id="edit-${postFromDB.key}" value="change"> <i class="fas fa-pencil-alt"></i> </button>
+      </footer>
+    </div>
+  </li>`);
+
+
+  $(`button[data-btn-id="btn-${postFromDB.key}"]`).click(function(){
+    database.ref("posts/" + USER_ID + "/" + postFromDB.key).remove();
+    $(this).parent().remove();
+  });
+
+  editPost(postFromDB.key);
+});
